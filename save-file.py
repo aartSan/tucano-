e80c3ae4963bcd62643c4e47e592bce3062ab9d8
Artem <sanchurskij@bk.ru>
2024-06-30 14:18:05 +0300

    bublik

--- save-file.py
+++
@@ -1,7 +0,0 @@
-{
-  "desk1": "банан",
-  "desk2": "ананас",
-  "desk3": "инжир",
-  "cards_count": "54"
-  "current_player_index": ,
-  "players": [
--- test file.py
+++
@@ -1,7 +0,0 @@
-class Hand:
-    def __init__(self):
-        self.cards = []
-#???def __repr__(self):
-        cards = []
-        for i in self.cards:
-            cards.append(i.value)
