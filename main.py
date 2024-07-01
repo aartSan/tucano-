2822eefc580433a60c448b500d5e90243eb8d3fd
Artem <sanchurskij@bk.ru>
2024-07-01 10:26:44 +0300

    bublik

--- main.py
+++
@@ -1,8 +0,0 @@
-from GameInteractions import GameInteractions
-
-if __name__ == '__main__':
-    # s = 0
-    # for v, k in Card.Card.CARD_COUNTS.items():
-    #     s += k
-    # print(s)
-    GameInteractions.run()
